b1075a8c53059d5afbfdb3c6065dacc766438511
nathan2s712 <nathan2s712@outlook.com>
2025-04-08 16:35:52 +1000

    Update using-dsg.js
    Personalised

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -7,13 +7,12 @@ import Seo from "../components/seo"
 const UsingDSG = () => (
   <Layout>
     <h1>
-      Hello from a <b>DSG Page</b>
+      Hello from Nathan
     </h1>
     <p>This page is not created until requested by a user.</p>
     <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
-        documentation about Deferred Static Generation
+      My LinkedIn Page{" "}
+      <a href="https://www.linkedin.com/in/nathananderson979797/">
       </a>
       .
     </p>
